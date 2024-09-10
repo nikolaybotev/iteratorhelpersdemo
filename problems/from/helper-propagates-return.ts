998aa0c0eaf45d14c31fba97a3eec7e6ead084f0
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-09-10 17:12:49 +0300

    More change

--- problems/from/helper-propagates-return.ts
+++ problems/from/helper-propagates-return.ts
@@ -6,7 +6,7 @@ const withHelpers = Iterator.from(generator);
 
 const result = withHelpers.next();
 
-const resultValue: number = result.value; // this should work!
+const resultValue: number = result.value; // this should work
 
 console.log(resultValue);  // 42
 console.log(result);  // { value: 42, done: true }
